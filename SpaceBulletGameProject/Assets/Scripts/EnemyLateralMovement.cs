65b38e9bd1e450df2d48fdaf3618b4b5eac1a1cf
EfeTigli <netigli@hotmail.com>
2022-07-05 14:52:17 +0300

    Create EnemyLateralMovement Script
    Thsi script enables enemy ship to make lateral movement between predefined positions with predefined speed.

---
+++ SpaceBulletGameProject/Assets/Scripts/EnemyLateralMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLateralMovement : MonoBehaviour
+{
+    [Header("Waypoints")]
+    [SerializeField] private Transform leftLimit;
+    [SerializeField] private Transform rightLimit;
+    private float distanceBetweenLimits;
+    private bool isMovingLeft;
+    private bool isMovingRight;
+
+    [Header("Flying Attribute")]
+    [SerializeField] private float speed;
+    private float distanceBetweenShipAndLimit;
+
+    private void Start()
+    {
+        isMovingRight = true;
+        isMovingLeft = false;
+        distanceBetweenLimits = Mathf.Abs(leftLimit.position.x - rightLimit.position.x);
+    }
+
+    private void Update()
+    {
+        Lateral();
+    }
+
+    private void Lateral()
+    {
+        if (isMovingLeft)
+        {
+            Debug.Log(distanceBetweenShipAndLimit + " " + distanceBetweenLimits);
+            distanceBetweenShipAndLimit = Mathf.Abs(this.transform.position.x - rightLimit.transform.position.x);
+            if (distanceBetweenShipAndLimit >= distanceBetweenLimits)
+            {
+                isMovingRight = true;
+                isMovingLeft = false;
+                return;
+            }
+
+            transform.position = transform.position + new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+        else if (isMovingRight)
+        {
+            Debug.Log(distanceBetweenShipAndLimit + " " + distanceBetweenLimits);
+            distanceBetweenShipAndLimit = Mathf.Abs(this.transform.position.x - leftLimit.transform.position.x);
+            if (distanceBetweenShipAndLimit >= distanceBetweenLimits)
+            {
+                isMovingRight = false;
+                isMovingLeft = true;
+                return;
+            }
+
+            Debug.Log("asd");
+            transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+    }
+
+}
