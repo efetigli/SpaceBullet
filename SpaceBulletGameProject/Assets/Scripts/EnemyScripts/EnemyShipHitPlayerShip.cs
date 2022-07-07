220bde7060a641e479f581b0aa44404736c12371
EfeTigli <netigli@hotmail.com>
2022-07-07 11:14:22 +0300

    Update EnemyShipHitPlayer Script
    If enemy ship hit player it triggers Game Over screen

--- SpaceBulletGameProject/Assets/Scripts/EnemyScripts/EnemyShipHitPlayerShip.cs
+++ SpaceBulletGameProject/Assets/Scripts/EnemyScripts/EnemyShipHitPlayerShip.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class EnemyShipHitPlayerShip : MonoBehaviour
 {
+    [Header("Game Over")]
+    [SerializeField] private GameObject gameOver;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            gameOver.SetActive(true);
             Destroy(collision.gameObject.transform.parent.gameObject);
         }
     }
