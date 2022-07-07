0b207716d55158d1203eee4c1bb5bdacc25256d5
EfeTigli <netigli@hotmail.com>
2022-07-07 12:59:51 +0300

    Create PauseMenu Script

---
+++ SpaceBulletGameProject/Assets/Scripts/ManagerScripts/PauseMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("Pause Menu")]
+    [SerializeField] private GameObject pauseMenu;
+
+    public void ClickPauseButton()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void ClickContinueButton()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+}
