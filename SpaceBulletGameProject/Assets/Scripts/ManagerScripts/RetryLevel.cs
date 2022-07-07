a8586d9ec7d29dfd3de09a099e814beb35bc491c
EfeTigli <netigli@hotmail.com>
2022-07-07 16:01:28 +0300

    Update RetryLevel Script

--- SpaceBulletGameProject/Assets/Scripts/ManagerScripts/RetryLevel.cs
+++ SpaceBulletGameProject/Assets/Scripts/ManagerScripts/RetryLevel.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class RetryLevel : MonoBehaviour
 {
+    [SerializeField] private float delayTime;
+
     public void ClickRetryLevelButton()
     {
+        StartCoroutine(DelayChangeScene());
+    }
+
+    private IEnumerator DelayChangeScene()
+    {
+        yield return new WaitForSeconds(delayTime);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
