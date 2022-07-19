312881d936f09ac8de241c04636d63d70e2d3b00
EfeTigli <netigli@hotmail.com>
2022-07-19 13:24:24 +0300

    Update EnemyExplode Script

--- SpaceBulletGameProject/Assets/Scripts/EnemyScripts/EnemyExplode.cs
+++ SpaceBulletGameProject/Assets/Scripts/EnemyScripts/EnemyExplode.cs
@@ -9,6 +9,8 @@ public class EnemyExplode : MonoBehaviour
 
     [SerializeField] private GameObject enemySprite;
 
+    [SerializeField] private AudioSource enemyExplodeSfx;
+
     public void ExplodeEnemyShip()
     {
         StartCoroutine(EnemyShipExplodeAnimation());
@@ -16,12 +18,16 @@ public class EnemyExplode : MonoBehaviour
 
     private IEnumerator EnemyShipExplodeAnimation()
     {
+        enemyExplodeSfx.enabled = false;
+
         enemyExplosion.SetActive(true);
 
         enemySprite.SetActive(false);
 
         this.GetComponent<EnemyLateralMovement>().enabled = false;
         this.GetComponent<EnemyForwardMovement>().enabled = false;
+
+        enemyExplodeSfx.enabled = true;
 
         yield return new WaitForSeconds(explosionTime);
         Destroy(this.gameObject);
