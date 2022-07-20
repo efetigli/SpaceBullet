3c012e51acd26a05e8edacfe63549a5456efe66f
EfeTigli <netigli@hotmail.com>
2022-07-20 13:35:02 +0300

    Create MuteGame Script

---
+++ SpaceBulletGameProject/Assets/Scripts/SoundScripts/MuteGame.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteGame : MonoBehaviour
+{
+    private bool isMute;
+
+    private void Start()
+    {
+        SavedMute();
+    }
+
+    private void SavedMute()
+    {
+        int m = PlayerPrefs.GetInt("Mute");
+        if (m == 1)
+        {
+            isMute = true;
+            AudioListener.pause = isMute;
+            this.GetComponent<Image>().color = Color.red;
+        }
+        else if (m == 0)
+        {
+            isMute = false;
+            AudioListener.pause = isMute;
+            this.GetComponent<Image>().color = Color.white;
+        }
+    }
+
+    public void MuteOrUnmuteGame()
+    {
+        if (isMute)
+        {
+            isMute = false;
+            AudioListener.volume = 1;
+            PlayerPrefs.SetInt("Mute", 0);
+            this.GetComponent<Image>().color = Color.white;
+        }
+        else if (!isMute)
+        {
+            isMute = true;
+            AudioListener.volume = 0;
+            PlayerPrefs.SetInt("Mute", 1);
+            this.GetComponent<Image>().color = Color.red;
+        }
+    }
+}
